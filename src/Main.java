c72752ac5a73358bbcb2368b18fb9822cf703c20
Dilys-web <opokudonkord@gmail.com>
2024-04-12 07:50:49 +0000

    updated main.java

--- src/Main.java
+++ src/Main.java
@@ -3,18 +3,20 @@ package src;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
+        final String message = "\nThis is a Babering Shop Java Program\nEnter space and press enter to create a new event\n";
         BarberingShop shop = new BarberingShop();
         Scanner sc = new Scanner(System.in);
-        System.out.println("Welcome to the barbershop!");
-        shop.displayHeader();
+        System.out.println(message);
+        ShopDisplay.displayHeader();
         while (true) {
             String input = sc.nextLine();
             if (!input.equals(" ")) {
                 break; // Break the loop if input is not a space
             }
             int x = new Random().nextInt(4); // generate a random integer between 0 and 3
-            shop.x = x;
+            shop.event = x;
             switch (x) {
                 case 3:
                 case 2:
